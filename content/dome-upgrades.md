ed438413200f19ab09c25bd5f072fc78cb09c6d1
kinneyj <kinneyj@u3c970e3c0587531cd3b0.ant.amazon.com>
2014-09-23 14:24:07 -0700

    Initial commit

---
+++ content/dome-upgrades.md
@@ -0,0 +1,8 @@
+Title: Upgrades to R2Beer2's Dome
+Date: 2014-09-23 10:20
+Category: Upgrades, Dome
+
+We recently made some changes to R2Beer2's dome. More pics of the process are on the way, but here is a quick shot of the installed dome:
+
+<a href="/images/new-dome-full.jpg"><img src="/images/new-dome-full.jpg" height=400 width=200 /></a>
+
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'R2Beer2 Team'
+SITENAME = u'R2Beer2'
+SITEURL = 'http://r2beer2.com'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Facebook', 'http://facebook.com/r2beer2bot'),
+         ('Twitter', 'http://twitter.com/r2beer2bot/'),
+         ('Flickr', 'http://flickr.com/photos/104774205@N04/'),)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'tuxlite_tbs'
+STATIC_PATHS = ['images']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
